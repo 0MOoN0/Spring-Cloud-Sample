041e99931dc8c6be2016b68a9f1afbce7907868b
0MOoN0 <814429032@qq.com>
2019-06-25 10:44:52 +0800

    [UPDATE/已测试]添加一个Eureka注册中心，并将DeptProvider注册到Eureka Eureka注册中心的端口为7001

--- microservicecloud-provider-dept-8001/src/main/java/com/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/springcloud/DeptProvider8001_App.java
@@ -2,11 +2,15 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ * DeptProvider提供类，Eureka客户端
+ *
  * @author Leon
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args)
     {
