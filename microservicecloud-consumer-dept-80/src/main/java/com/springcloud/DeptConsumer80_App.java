39d1398638319fd2be11c1096835464243572b20
0MOoN0 <814429032@qq.com>
2019-06-28 10:15:44 +0800

    [UPDATE/已测试]使用Ribbon配置客户端负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
@@ -2,11 +2,13 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Leon
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
--- microservicecloud-consumer-dept-80/src/main/java/com/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.springcloud.cfgbeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
+    @LoadBalanced //负载均衡
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
