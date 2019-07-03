7908eba129e8025a7c3ee6f44946c2e518feaba8
0MOoN0 <814429032@qq.com>
2019-07-03 10:44:58 +0800

    [UPDATE/已测试]指定Provider服务的Ribbon负载均衡规则

--- microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/springcloud/DeptConsumer80_App.java
@@ -1,14 +1,18 @@
 package com.springcloud;
 
+import com.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 /**
  * @author Leon
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
